7bb4a6ba99bd5147c9afbe8a04e7e84ea9af6386
Maksym <r4v3c4t@pm.me>
2024-10-14 15:33:05 +0300

    cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./app/store.ts";
+import { store } from "./app/store";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
